5a35c222e9765d4d342da5fef577159fe369fed4
Huang_Baike <splaueef@pm.me>
2026-03-15 05:36:38 +0100

    TEST

---
+++ README.md
@@ -0,0 +1,111 @@
+<div align="center">
+
+# Hikka (Custom Build)
+
+<img src="https://img.shields.io/badge/status-unofficial%20fork-orange">
+<img src="https://img.shields.io/badge/build-custom-blue">
+<img src="https://img.shields.io/badge/python-3.8+-blue">
+
+<br>
+
+<a href="https://deepsource.io/gh/Splaueef/Hikka/?ref=repository-badge">
+<img src="https://deepsource.io/gh/Splaueef/Hikka.svg/?label=active+issues&show_trend=true&token=IPVI_QX-cSuQSVeVl8cb5PLt" alt="DeepSource">
+</a>
+
+<a href="https://deepsource.io/gh/Splaueef/Hikka/?ref=repository-badge">
+<img src="https://deepsource.io/gh/Splaueef/Hikka.svg/?label=resolved+issues&show_trend=true&token=IPVI_QX-cSuQSVeVl8cb5PLt" alt="DeepSource">
+</a>
+
+<br>
+
+<a href="https://www.codacy.com/gh/Splaueef/Hikka/dashboard">
+<img src="https://app.codacy.com/project/badge/Grade/97e3ea868f9344a5aa6e4d874f83db14"/>
+</a>
+
+<img src="https://img.shields.io/github/languages/code-size/Splaueef/Hikka"/>
+<img src="https://img.shields.io/github/issues-raw/Splaueef/Hikka"/>
+<img src="https://img.shields.io/github/license/Splaueef/Hikka"/>
+<img src="https://img.shields.io/github/commit-activity/m/Splaueef/Hikka"/>
+
+<br>
+
+<img src="https://img.shields.io/github/forks/Splaueef/Hikka?style=flat"/>
+<img src="https://img.shields.io/github/stars/Splaueef/Hikka"/>
+
+<a href="https://github.com/psf/black">
+<img src="https://img.shields.io/badge/code%20style-black-000000.svg">
+</a>
+
+</div>
+
+---
+
+## Notice
+
+This repository is **NOT an official build of Hikka**.
+
+It is a **"fork"** (the word is intentionally in quotes), because the repository was **not created using GitHub's fork system**.  
+Instead, the original repository was **copied manually and uploaded as a separate project**.
+
+The purpose of this repository is to provide a **personal modified build** with various adjustments and improvements for convenience.
+
+### Important clarification
+
+• I **do NOT claim ownership** of the original Hikka project  
+• All **core credits belong to the original developers and contributors**  
+• This repository exists only as a **custom distribution**
+
+This repository may include:
+
+• configuration changes  
+• modified links and resources  
+• custom fixes  
+• experimental patches  
+• personal improvements  
+
+Over time, this repository may also include **independent updates that do not exist in the original project**.
+
+If you want the **official project**, please refer to the original Hikka sources.
+
+
+This project is a modified version of Hikka.
+Original project: https://github.com/hikariatama/Hikka
+Licensed under AGPL-3.0.
+
+
+ The original Hikka project is currently archived and no longer actively maintained.
+ This repository exists to keep the project usable and functional. 
+
+
+
+---
+
+## Warning
+
+If you are a paranoid person, you should not use this userbot.
+
+This userbot is not a virus, but it can be used for malicious purposes.  
+You are responsible for all actions taken by your account.
+
+---
+
+## Installation
+
+### Installation page
+
+<img src="https://github.com/Splaueef/assets/raw/main/install_qr.gif" height="256">
+
+<a href="https://t.me/lavhostbot?start=SGlra2E">
+<img src="https://user-images.githubusercontent.com/36935426/167272288-85f00779-4b98-47da-8d0d-ea2c6370b979.png" height="40">
+</a>
+
+---
+
+### Manual installation
+
+```bash
+apt update && apt install git libcairo2 -y
+git clone https://github.com/Splaueef/hikka
+cd Hikka
+pip install -r requirements.txt
+python3 -m hikka
---
+++ hikka/modules/notes.py
@@ -0,0 +1,152 @@
+# 	Friendly Telegram (telegram userbot)
+# 	Copyright (C) 2018-2019 The Authors
+
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU Affero General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU Affero General Public License for more details.
+
+# 	You should have received a copy of the GNU Affero General Public License
+# 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+
+from .. import loader, utils
+
+logger = logging.getLogger("friendly-telegram.modules.notes")
+
+
+@loader.tds
+class NotesMod(loader.Module):
+    """Зберігає глобальні нотатки (також відомі як «snips»)."""
+    strings = {
+        "name": "Notes",
+        "what_note": "<b>Яку нотатку потрібно показати?</b>",
+        "no_note": "<b>Нотатку не знайдено</b>",
+        "save_what": "<b>Що потрібно зберегти?</b>",
+        "what_name": "<b>Як буде називатися нотатка?</b>",
+        "saved": "<b>Нотатку збережено як:</b> <code>{}</code>",
+        "notes_header": "<b>Збережені нотатки:</b>\n\n",
+        "notes_item": "<b>▷</b> <code>{}</code>",
+        "delnote_args": "<b>Яку нотатку потрібно видалити?</b>",
+        "delnote_done": "<b>Нотатку видалено!</b>",
+        "delnotes_none": "<b>Нотаток немає...</b>",
+        "delnotes_done": "<b>УСІ НОТАТКИ ВИДАЛЕНО</b>",
+        "notes_none": "<b>Нотаток немає...</b>",
+    }
+
+    async def findnotecmd(self, message):
+        """Отримує вказану нотатку."""
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, self.strings("what_note", message))
+            return
+        asset_id = self._db.get("friendly-telegram.modules.notes", "notes", {}).get(
+            args[0], None
+        )
+        logger.debug(asset_id)
+        asset = await self._db.fetch_asset(asset_id) if asset_id is not None else None
+        if asset is None:
+            self.del_note(args[0])
+            await utils.answer(message, self.strings("no_note", message))
+            return
+        link = "https://t.me/c/{}/{}".format(asset.chat.id, asset.id)
+        await message.edit(
+            f'<b>Нотатка</b> "<code>{args[0]}</code>" <a href="{link}">знаходиться тут.</a>'
+        )
+
+    async def notecmd(self, message):
+        """Отримує вказану нотатку."""
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, self.strings("what_note", message))
+            return
+        asset_id = self._db.get("friendly-telegram.modules.notes", "notes", {}).get(
+            args[0], None
+        )
+        logger.debug(asset_id)
+        asset = await self._db.fetch_asset(asset_id) if asset_id is not None else None
+        if asset is None:
+            self.del_note(args[0])
+            await utils.answer(message, self.strings("no_note", message))
+            return
+        await message.delete()
+        await message.client.send_message(
+            message.to_id,
+            await self._db.fetch_asset(asset_id),
+            reply_to=await message.get_reply_message(),
+        )
+
+    async def delallnotescmd(self, message):
+        """Видаляє всі збережені нотатки."""
+        if not self._db.get("friendly-telegram.modules.notes", "notes", {}):
+            await utils.answer(message, self.strings("delnotes_none", message))
+            return
+        self._db.get("friendly-telegram.modules.notes", "notes", {}).clear()
+        await utils.answer(message, self.strings("delnotes_done", message))
+
+    async def savecmd(self, message):
+        """Зберігає нову нотатку. Потрібно використовувати у відповіді на повідомлення з одним параметром (назва нотатки)."""
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, self.strings("what_name", message))
+            return
+        if message.is_reply:
+            target = await message.get_reply_message()
+        elif len(args) < 2:
+            await utils.answer(message, self.strings("save_what", message))
+            return
+        else:
+            message.entities = None
+            message.message = args[1]
+            target = message
+            logger.debug(target.message)
+        asset_id = await self._db.store_asset(target)
+        self._db.set(
+            "friendly-telegram.modules.notes",
+            "notes",
+            {
+                **self._db.get("friendly-telegram.modules.notes", "notes", {}),
+                args[0]: asset_id,
+            },
+        )
+        await utils.answer(message, str(self.strings("saved", message)).format(args[0]))
+
+    async def delnotecmd(self, message):
+        """Видаляє нотатку, вказану за назвою."""
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, self.strings("delnote_args", message))
+        self.del_note(args[0])
+        await utils.answer(message, self.strings("delnote_done", message))
+
+    def del_note(self, note):
+        old = self._db.get("friendly-telegram.modules.notes", "notes", {})
+        try:
+            del old[note]
+        except KeyError:
+            pass
+        else:
+            self._db.set("friendly-telegram.modules.notes", "notes", old)
+
+    async def notescmd(self, message):
+        """Показує список збережених нотаток."""
+        if not self._db.get("friendly-telegram.modules.notes", "notes", {}):
+            await utils.answer(message, self.strings("notes_none", message))
+            return
+        await utils.answer(
+            message,
+            self.strings("notes_header", message)
+            + "\n".join(
+                self.strings("notes_item", message).format(key)
+                for key in self._db.get("friendly-telegram.modules.notes", "notes", {})
+            ),
+        )
+
+    async def client_ready(self, client, db):
+        self._db = db
